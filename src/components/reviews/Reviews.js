9bbe5e878487dc923feb3a8b8551285a8b3360f6
Bilal-Habib <dev.bilal01@gmail.com>
2024-04-25 14:23:16 +0100

    added get request to get all reviews as the backend now connects to dynamodb instead of mongo

---
+++ src/components/reviews/Reviews.js
@@ -0,0 +1,94 @@
+import { useEffect, useRef } from "react";
+import api from "../../api/axiosConfig";
+import { useParams } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import ReviewForm from "../reviewForm/ReviewForm";
+
+import React from "react";
+
+const Reviews = ({ getMovieData, movie, reviews, setReviews }) => {
+  const revText = useRef();
+  const params = useParams();
+  const imdbId = params.movieId;
+
+  useEffect(() => {
+    getMovieData(imdbId);
+  }, [imdbId]);
+
+  const addReview = async (e) => {
+    e.preventDefault();
+
+    try {
+      const rev = revText.current;
+
+      const response = await api.post("/reviews", {
+        reviewBody: rev.value,
+        imdbId: imdbId,
+      });
+
+      const updatedReviews = [...reviews, { body: rev.value }];
+
+      rev.value = "";
+
+      setReviews(updatedReviews);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h3>Reviews</h3>
+        </Col>
+      </Row>
+      <Row className="mt-2">
+        <Col>
+          <img src={movie?.poster} />
+        </Col>
+        <Col>
+          {
+            <>
+              <Row>
+                <Col>
+                  <ReviewForm
+                    handleSubmit={addReview}
+                    revText={revText}
+                    labelText="Write a Review?"
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <hr />
+                </Col>
+              </Row>
+            </>
+          }
+          {reviews?.map((review, index) => {
+            return (
+              <div key={index}>
+                <Row>
+                  <Col>{review.body}</Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <hr />
+                  </Col>
+                </Row>
+              </div>
+            );
+          })}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <hr />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Reviews;
---
+++ src/components/trailer/Trailer.js
@@ -0,0 +1,25 @@
+import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import "./Trailer.css";
+
+import React from "react";
+
+const Trailer = () => {
+  const params = useParams();
+  const ytTrailerId = params.ytTrailerId;
+  return (
+    <div className="react-player-container">
+      {ytTrailerId != null ? (
+        <ReactPlayer
+          controls="true"
+          playing={true}
+          url={`https://www.youtube.com/watch?v=${ytTrailerId}`}
+          width="100%"
+          height="100%"
+        />
+      ) : null}
+    </div>
+  );
+};
+
+export default Trailer;
